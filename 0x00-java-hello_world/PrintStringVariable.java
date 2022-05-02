6dc27ab7383814a88855752366783acc1d932d3a
hyginocit <gnieves@ciandt.com>
2022-05-02 13:03:27 -0300

    exercicio 3

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+     public static void main ( String args []) {
+        String Str = new String("Holberton School");
+        System.out.println(Str); System.out.println(Str);
+        System.out.println(Str.substring(1, 9));
+    }}
