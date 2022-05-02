f6bff6321e7a1a4de6b72f3fa9c5304d0cb57a1c
hyginocit <gnieves@ciandt.com>
2022-05-02 13:17:42 -0300

    exercicio 2

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args){
-        float pi = 3.14159;
-        System.out.printf("'%3.3f'%n", pi);
+        float PI = (float)Math.PI;
+        System.out.printf("Valor de pi: %.3f%n" + pi);
     }
 }
