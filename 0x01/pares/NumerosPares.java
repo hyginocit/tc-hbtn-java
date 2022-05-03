6181ac29196f0da674c5692fb20a04522698946b
hyginocit <gnieves@ciandt.com>
2022-05-03 16:04:28 -0300

    atividade 01

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,14 @@
+public class NumeroPares {
+        public static void main(String[] args) {
+            for (int i = 0; i < 98; i++){
+
+                if (i %2 == 0){
+                    System.out.print(i + ", ");
+                }
+
+                if (i > 96){
+                    System.out.println("98");
+                }
+            }
+        }
+}
