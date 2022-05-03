b5d7c734e815ef3eeda4179e7dba7940b0ac5977
hyginocit <gnieves@ciandt.com>
2022-05-03 16:45:56 -0300

    atividade 04

--- 0x01/somar/Numero.java
+++ 0x01/somar/Numero.java
@@ -1,4 +1,9 @@
 public class Numero {
-    public static int somar(int a, int b){
+    public static void main(String[] args) {
+
+
+    }
+    public static int somar(int a, int b) {
         return (a + b);
-    }}
+    }
+}
