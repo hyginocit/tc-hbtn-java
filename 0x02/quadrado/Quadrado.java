35f547bf7489bf0cf93fcb17e1c9712fc831964b
hyginocit <104362153+hyginocit@users.noreply.github.com>
2022-05-04 16:55:13 -0300

    Update Quadrado.java

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Quadrado {
+public class Quadrado {
+    public static double area(double lado) {
+        double result = 0;
+        try {
+             result = lado * lado;
+            if (lado < 0) {
+                throw new IllegalArgumentException();
+            }
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Lado deve possuir valor positivo");
+
+        }
+        return result;
+    }
+
 }
