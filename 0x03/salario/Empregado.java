c9112be60db1b0a6a2d84a227cc4353bf03c63c5
hyginocit <gnieves@ciandt.com>
2022-05-06 16:21:35 -0300

    atividade 00

--- 0x03/salario/Empregado.java
+++ 0x03/salario/Empregado.java
@@ -2,7 +2,11 @@ public class Empregado {
     private double salarioFixo;
 
     public double calcularBonus(Departamento departamento){
+        if (departamento.getValorAtingidoMeta() >= departamento.getValorMeta()){
          return salarioFixo * 0.1;
+        } else {
+            return 0;
+        }
     }
     public double calcularSalarioTotal(Departamento departamento){
         return salarioFixo + salarioFixo * 0.1;
