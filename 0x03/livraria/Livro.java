aaca17bca0736b6b9ada5d3567ef74f26234bf55
hyginocit <gnieves@ciandt.com>
2022-05-09 12:05:04 -0300

    atividade 02

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,54 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) throws AutorInvalidoException, LivroInvalidoException {
+        Livro livro = new Livro();
+        this.titulo = livro.getTitulo();
+        this.autor = livro.getAutor();
+        this.preco = livro.getPreco();
+        this.setTitulo(titulo);
+        this.setAutor(autor);
+        this.setPreco(preco);
+    }
+
+    public Livro() {
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) throws AutorInvalidoException {
+        if (!autor.contains(" ")) {
+            throw new AutorInvalidoException("Nome de autor invalido");
+        }
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) throws LivroInvalidoException {
+        if (preco <= 0) {
+            throw new LivroInvalidoException("Preco de livro invalido");
+        }
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) throws LivroInvalidoException {
+        if (titulo.length() < 3 || titulo.isEmpty()) {
+            throw new LivroInvalidoException("Titulo de livro invalido");
+        }
+        this.titulo = titulo;
+    }
+}
