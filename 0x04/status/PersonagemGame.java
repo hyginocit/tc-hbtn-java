b14191667ac963f38acc44d41448b30334cd1da5
hyginocit <gnieves@ciandt.com>
2022-05-09 17:47:24 -0300

    atividade 03

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,53 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if (this.saudeAtual > 0){
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int tomarDano(int quantidadeDeDano){
+        this.saudeAtual -= quantidadeDeDano;
+        if (this.saudeAtual <= 0) {
+            this.saudeAtual = 0;
+        }
+        setSaudeAtual(saudeAtual);
+        return this.saudeAtual;
+    }
+
+
+    public int receberCura(int quantidadeDeCura){
+        this.saudeAtual += quantidadeDeCura;
+        if (this.saudeAtual > 100) {
+            this.saudeAtual = 100;
+        }
+
+        setSaudeAtual(this.saudeAtual);
+        return this.saudeAtual;
+    }
+}
