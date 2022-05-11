e6031035c2bcaea40eb21267da6bef0952f8e83a
hyginocit <gnieves@ciandt.com>
2022-05-11 16:05:11 -0300

    atividade 01

---
+++ 0x06/resumo_pedido/ItemPedido.java
@@ -0,0 +1,32 @@
+import produtos.Produto;
+
+public class ItemPedido {
+
+    private int quantidade;
+
+    private Produto produto;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public ItemPedido() {
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
