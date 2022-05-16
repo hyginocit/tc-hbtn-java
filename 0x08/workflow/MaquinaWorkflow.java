c647a869a24af118f6ae6d5762727d89a61e2b73
hyginocit <gnieves@ciandt.com>
2022-05-16 17:47:41 -0300

    atividade 00

---
+++ 0x08/workflow/MaquinaWorkflow.java
@@ -0,0 +1,9 @@
+import atividades.Atividade;
+
+public class MaquinaWorkflow {
+    void executar(Workflow workflow) {
+        for (Atividade atividade : workflow.getAtividades()){
+            atividade.executar();
+        }
+    }
+}
---
+++ 0x08/workflow/Workflow.java
@@ -0,0 +1,28 @@
+import atividades.Atividade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Workflow {
+
+    private List<Atividade> atividades;
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(List<Atividade> atividades) {
+        this.atividades = atividades;
+    }
+
+    public Workflow(){
+        this.atividades = new ArrayList<>();
+    }
+
+    void registrarAtividade(Atividade atividade){
+        this.atividades.add(atividade);
+    }
+
+
+}
