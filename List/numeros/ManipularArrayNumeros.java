92813fe987a31e28385ac98ea9852289aa659f3e
hyginocit <gnieves@ciandt.com>
2022-05-20 10:09:33 -0300

    atividade 00

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class ManipularArrayNumeros {
+    public static int buscarPosicaoNumero(List<Integer> numeros, int numero) {
+
+        return numeros.indexOf(numero);
+    }
+
+    public static void adicionarNumero(List<Integer> numeros, int numero){
+        int contem = buscarPosicaoNumero(numeros, numero);
+        if (contem == -1){
+            numeros.add(numero);
+        } else {
+            throw new IllegalArgumentException("Numero jah contido na lista");
+        }
+    }
+
+    public static void removerNumero(List<Integer> numeros, int numero){
+        int contem = buscarPosicaoNumero(numeros, numero);
+        if (contem == -1){
+            numeros.remove(contem);
+        } else {
+            throw new IllegalArgumentException("Numero nao encontrado na lista");
+        }
+    }
+    public static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto){
+
+        int exists = buscarPosicaoNumero(numeros, numeroSubstituir);
+
+        if (exists != -1) {
+            numeros.set(exists, numeroSubstituto);
+        } else {
+            numeros.add(numeroSubstituto);
+        }
+    }
+}
+
