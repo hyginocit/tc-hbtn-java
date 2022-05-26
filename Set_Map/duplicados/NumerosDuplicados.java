68db0c4d18b1109d41b5122b2a3b1ec69a1213d5
hyginocit <gnieves@ciandt.com>
2022-05-26 09:33:57 -0300

    atividade 01

---
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class NumerosDuplicados {
+    public static TreeSet<Integer> buscar(int[] numeros) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int numero : numeros) {
+            list.add(numero);
+        }
+
+        Set<Integer> numerosDuplicados = new HashSet<>();
+
+        for (int numero : list) {
+            if (Collections.frequency(list, numero) > 1) {
+                numerosDuplicados.add(numero);
+            }
+        }
+
+        return new TreeSet<>(numerosDuplicados);
+    }
+}
