81b607e28d2aabb4b151c0767f85aac3129004bf
hyginocit <gnieves@ciandt.com>
2022-05-30 19:26:15 -0300

    atividade 00

--- Collections_Exercicios/blog/Blog.java
+++ Collections_Exercicios/blog/Blog.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Blog {
+import java.util.*;
+
+public class Blog {
+
+    private List<Post> posts = new ArrayList<>();
+    private Set<String> autores = new TreeSet<>();
+
+    //private String autores;
+
+    public void adicionarPostagem(Post postagem){
+        this.posts.add(postagem);
+    }
+
+    public TreeSet<String> obterTodosAutores(){
+        for(Post post : posts){
+            autores.add(post.getAutor());
+            //autores = post.getAutor();
+        }
+        return (TreeSet<String>) autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        ArrayList<String> categorias = new ArrayList<>();
+        Map<String, Integer> countCategorias = new TreeMap<>();
+
+        for(Post post : posts){
+            categorias.add(post.getCategoria());
+        }
+
+        for (String categoria : categorias){
+            if (!countCategorias.containsKey(categoria)){
+                countCategorias.put(categoria, 1);
+            }else {
+                countCategorias.put(categoria, countCategorias.get(categoria)+1);
+            }
+        }
+        return countCategorias;
+    }
 }
