1401a5957e6ce6900c9a4d06180997ce8de4a1ed
hyginocit <gnieves@ciandt.com>
2022-06-09 08:31:20 -0300

    atividade 00

--- Lambda/markup/Produto.java
+++ Lambda/markup/Produto.java
@@ -1,23 +1,19 @@
 
+
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Produto {
-
     private String nome;
+    private Double preco;
+    private Double percentualMarkUp = 0.1;
 
-    private double preco;
-
-    private double percentualMarkUp = 0.10;
-
-    public Supplier<Double> precoComMarkUp = () -> preco + (preco * percentualMarkUp);
-
-    public Consumer<Double> atualizarMarkUp = (novoMarkUp) -> percentualMarkUp = (novoMarkUp / 100);
-
-    public Produto(double preco, String nome) {
+    public Produto(double preco,String nome) {
         this.preco = preco;
         this.nome = nome;
     }
+    Supplier<Double> precoComMarkUp = () -> preco*(1+percentualMarkUp);
+    Consumer<Double> atualizarMarkUp = x -> this.percentualMarkUp = (x/100);
 
     public String getNome() {
         return nome;
@@ -30,4 +26,17 @@ public class Produto {
     public double getPreco() {
         return preco;
     }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public double getPercentualMarkUp() {
+        return percentualMarkUp;
+    }
+
+    public void setPercentualMarkUp(Double percentualMarkUp) {
+        this.percentualMarkUp = percentualMarkUp;
+    }
+
 }
